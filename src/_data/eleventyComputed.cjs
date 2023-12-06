adfe390932bba6df549a2cedd317ebac58c54c25
Carter Medlin <carterm@users.noreply.github.com>
2023-12-06 13:00:41 -0800

    everything

--- src/_data/eleventyComputed.cjs
+++ src/_data/eleventyComputed.cjs
@@ -1,6 +1,25 @@
 //@ts-check
 
+// The default front-matter used for every page
+
 module.exports = {
-  description: data => data.description || "default description",
-  title: data => data.title || "default title"
+  /** @param {globaldata} _data} */
+  description: _data => _data.description || "default description",
+  /** @param {globaldata} _data} */
+  title: _data => _data.title || "default title",
+
+  // uncomment to have a var to see all the available data
+  everything: (/** @type {globaldata} */ _data) =>
+    JSON.stringify(_data, null, 2)
 };
+
+/**
+ * @typedef {Object} globaldata
+ * @property {string} description
+ * @property {string} title
+ * @property {string} layout
+ * @property {{}} eleventy
+ * @property {{}} pkg
+ * @property {{}} page
+ * @property {{}} collections
+ */
