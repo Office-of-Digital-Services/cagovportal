440c31d7d1e10c2a9134360a4bc03a3578ba2f50
Carter Medlin <carterm@users.noreply.github.com>
2023-12-08 12:48:23 -0800

    using mutation observer

--- pages/agency.js
+++ pages/agency.js
@@ -1,15 +1,39 @@
 //@ts-check
 
 window.addEventListener("DOMContentLoaded", () => {
+  const search = /** @type {HTMLInputElement} */ (
+    document.querySelector("#agency_search")
+  );
+  const result_count = /** @type {HTMLElement} */ (
+    document.querySelector("#result_count")
+  );
+
+  const result_list = /** @type {HTMLUListElement} */ (
+    document.querySelector("#result_list")
+  );
+
+  // Callback function to execute when mutations are observed
+  /** @type {MutationCallback} */
+  const callback = () => {
+    const count = result_list.querySelectorAll(
+      'agency-row:not([style*="display: none"])'
+    ).length;
+    console.log(count);
+    result_count.innerText = count.toString();
+  };
+
+  // Create an observer instance linked to the callback function
+  // Start observing the target node for configured mutations
+  new MutationObserver(callback).observe(result_list, {
+    subtree: true,
+    attributeFilter: ["aria-hidden"]
+  });
+
   customElements.define(
     "agency-row",
     class extends HTMLElement {
       constructor() {
         super();
-        let template = this.querySelector("template");
-
-        const shadowRoot = this.attachShadow({ mode: "closed" });
-        shadowRoot.appendChild(template.content.cloneNode(true));
 
         /** @type {string} */
         this.words = /** @type {string} */ (this.attributes["keywords"].value)
@@ -18,19 +42,17 @@ window.addEventListener("DOMContentLoaded", () => {
 
         this.checkme = (/** @type {Event} */ event) => {
           const value = /** @type {HTMLInputElement} */ (event.target).value
-            .trim()
+            //.trim() not trimming on purpose
             .toLowerCase();
 
-          this.style.display =
-            !value || this.words.includes(value) ? "" : "none";
+          const bShow = !value || this.words.includes(value);
+
+          this.style.display = bShow ? "" : "none";
+          this.ariaHidden = (!bShow).toString();
         };
       }
 
       connectedCallback() {
-        const search = /** @type {HTMLInputElement} */ (
-          document.querySelector("#agency_search")
-        );
-
         search.addEventListener("input", this.checkme);
       }
     }
