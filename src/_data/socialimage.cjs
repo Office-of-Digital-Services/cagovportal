a3810dcf5eaa4056159a2b53462c52f8687394bb
Carter Medlin <carterm@users.noreply.github.com>
2024-03-19 13:25:16 -0700

    provider logo url

---
+++ src/_data/socialimage.cjs
@@ -0,0 +1,7 @@
+//@ts-check
+
+// Default social media image
+// Can't be added to eleventyComputed.cjs, needs to be available early
+
+module.exports =
+  "https://california.azureedge.net/cdt/statetemplate/global/images/social_media_preview_image.png";
