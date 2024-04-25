765eff38d7025df2019e88c2f3d8fa6264988257
Carter Medlin <carterm@users.noreply.github.com>
2024-04-25 08:24:01 -0700

    checkbox filters work

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -7,7 +7,6 @@ window.addEventListener("DOMContentLoaded", () => {
     "cagovhome-filterlist",
     class extends HTMLElement {
       connectedCallback() {
-        console.log(this.dataset);
         const inputid = this.dataset.searchInputId;
         if (!inputid) {
           throw new Error("Missing data-search-input-id");
@@ -39,17 +38,38 @@ window.addEventListener("DOMContentLoaded", () => {
         }
 
         /** @type {*[]} */
-        const dataset = JSON.parse(datasetstring);
+        const storageData = JSON.parse(datasetstring);
 
-        const elementRows = [
-          .../** @type {NodeListOf<HTMLElement>} */ (
-            this.querySelectorAll("[data-row-key]")
-          )
-        ];
+        const elementRows = /** @type {HTMLElement[]} */ ([
+          ...this.querySelectorAll("[data-row-key]")
+        ]);
 
         if (!elementRows.length) {
           throw new Error(`can't find any elements with "data-row-key"`);
         }
+
+        const filterTriggerSelector = this.dataset.filterTriggerSelector;
+        const triggerElements = /** @type {HTMLInputElement[]} */ ([
+          ...document.querySelectorAll(filterTriggerSelector)
+        ]);
+
+        if (filterTriggerSelector && !triggerElements.length) {
+          throw new Error(
+            `No elements found with data-filter-trigger-selector (${filterTriggerSelector})`
+          );
+        }
+
+        triggerElements.forEach(x => {
+          if (x.tagName !== "INPUT")
+            throw new Error(
+              "Only INPUT elements are allowed in data-filter-trigger-selector"
+            );
+
+          if (!storageData.some(d => d[x.name]))
+            throw new Error(
+              `Trigger Element ${x.id} has a NAME (${x.name}) that is not in the data`
+            );
+        });
 
         const keyValues = [...new Set(elementRows.map(x => x.dataset.rowKey))];
 
@@ -62,21 +82,26 @@ window.addEventListener("DOMContentLoaded", () => {
           //filter the data for the keys that match the search value
           const keyMatches = keyValues.filter(key => {
             // grab a row from the dataset that matches the key
-            const datarow = dataset.find(x => x[keyProperty] == key);
+            const datarow = storageData.find(x => x[keyProperty] == key);
 
             if (!datarow) {
               throw new Error(
-                `key not found in data storage - ${keyProperty}:${key}`
+                `data-row-key not found in data storage - ${keyProperty}:${key}`
               );
             }
 
             // join all the dataset values for this row together for general search
             const alldata = Object.values(datarow).join(" ");
 
-            return (
+            const textBoxMatches =
               !value ||
-              (alldata || "").replace(/\W/g, " ").toLowerCase().includes(value)
+              (alldata || "").replace(/\W/g, " ").toLowerCase().includes(value);
+
+            const filtterTriggersMatch = triggerElements.every(
+              x => !x.checked || datarow[x.name].includes(x.value)
             );
+
+            return textBoxMatches && filtterTriggersMatch;
           });
 
           // Apply test to count displays
@@ -94,6 +119,10 @@ window.addEventListener("DOMContentLoaded", () => {
         };
 
         inputBox.addEventListener("input", checkme);
+
+        //connect the triggers
+        triggerElements.forEach(x => x.addEventListener("change", checkme));
+
         checkme();
       }
     }
