46c91050bc2d8468502fe4164ba9c76d2953bc46
kkoryaka <konstantin.koryaka@state.ca.gov>
2024-06-11 15:00:36 -0700

    updated URLS and social image

--- src/_data/socialimage.cjs
+++ src/_data/socialimage.cjs
@@ -4,4 +4,4 @@
 // Can't be added to eleventyComputed.cjs, needs to be available early
 
 module.exports =
-  "https://cdn.cdt.ca.gov/cdt/statetemplate/global/images/social_media_preview_image.png";
+  "https://cdn.cdt.ca.gov/CAgovPortal/images/ca-gov-social-sharing.jpg";
