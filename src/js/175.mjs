33741d3d20a64867f4b4f68ce1b819f2641da4c0
kkoryaka <konstantin.koryaka@state.ca.gov>
2024-09-06 14:45:44 -0700

    updated language

--- src/js/175.mjs
+++ src/js/175.mjs
@@ -2,6 +2,55 @@
 
 //@ts-check
 
+/* sticky header / hiding official header on scroll */
+window.addEventListener("load", () => {
+  const doc = document.documentElement;
+
+  let prevScroll = window.scrollY || doc.scrollTop;
+  let curScroll;
+  let direction = 0;
+  let prevDirection = 0;
+
+  const headerAlert = document.querySelector("header .alert");
+  const header = document.querySelector(".utility-header");
+  const mainheader = document.querySelector("header");
+  if (!header || !mainheader) return;
+
+  window.addEventListener("scroll", () => {
+    /*
+     ** Find the direction of scroll
+     ** 0 - initial, 1 - up, 2 - down
+     */
+
+    curScroll = window.scrollY || doc.scrollTop;
+    if (curScroll > prevScroll) {
+      //scrolled up
+      direction = 2;
+    } else if (curScroll < prevScroll) {
+      //scrolled down
+      direction = 1;
+    }
+
+    if (direction !== prevDirection) {
+      // Toggle Header
+      if (direction === 2 && curScroll > 40) {
+        const hiddenheight =
+          header.clientHeight + (headerAlert?.clientHeight || 0);
+
+        mainheader.style.top = `-${hiddenheight}px`;
+        prevDirection = direction;
+      } else if (direction === 1 && curScroll < 40) {
+        // mainheader.classList.remove('scrolled');
+        // header.classList.remove('is-hidden');
+        // header.removeAttribute("style");
+        mainheader.style.removeProperty("top");
+        prevDirection = direction;
+      }
+    }
+
+    prevScroll = curScroll;
+  });
+});
 
 // retain scroll position
 
