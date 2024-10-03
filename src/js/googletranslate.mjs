bc3f6bd7d28d50a96622ae67a4bb5707b9654c2d
Carter Medlin <carterm@users.noreply.github.com>
2024-10-03 13:23:55 -0700

    translations fix

--- src/js/googletranslate.mjs
+++ src/js/googletranslate.mjs
@@ -2,25 +2,14 @@
 
 // Google Translate support
 window.addEventListener("DOMContentLoaded", () => {
-  const loadGoogle = () => {
-    window["googleTranslateElementInit"] = () => {
-      new window["google"].translate.TranslateElement({
-        pageLanguage: "en",
-        includedLanguages: "en,es,ko,tl,vi,zh-TW"
-      });
-    };
-
-    // Add translate script to page
-    const script = document.createElement("script");
-    script.src =
-      "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-    document.head.appendChild(script);
-  };
-
-  // Change the way the translate links work to do a reload
+  // trigger a refresh after the translate link changes the page hash
   document
     .querySelectorAll(".settings-links > a")
-    .forEach(l => l.addEventListener("click", loadGoogle));
+    .forEach(l =>
+      l.addEventListener("click", () =>
+        window.addEventListener("hashchange", () => window.location.reload())
+      )
+    );
 
   // Function to get a cookie by name using modern JavaScript
   /**
@@ -37,6 +26,17 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // Check for the "googtrans" cookie or #googtrans URL fragment
   if (getCookie("googtrans") || window.location.hash.includes("#googtrans")) {
-    loadGoogle();
+    window["googleTranslateElementInit"] = () => {
+      new window["google"].translate.TranslateElement({
+        pageLanguage: "en",
+        includedLanguages: "en,es,ko,tl,vi,zh-TW"
+      });
+    };
+
+    // Add translate script to page
+    const script = document.createElement("script");
+    script.src =
+      "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    document.head.appendChild(script);
   }
 });
