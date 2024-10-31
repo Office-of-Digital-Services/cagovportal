827c132ccbd0897543673e6a108306f69afeebf1
Carter Medlin <carterm@users.noreply.github.com>
2024-10-31 11:36:21 -0700

    polyfill separate file

--- src/js/polyfills.mjs
+++ src/js/polyfills.mjs
@@ -1,26 +1,7 @@
 //@ts-check
 
-function supportsWebP(callback) {
-  const webP = new Image();
-  webP.onload = webP.onerror = function () {
-    callback(webP.height === 1);
-  };
-  webP.src =
-    "data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=";
-}
-
-function replaceWebPWithPNG() {
-  const images = /** @type {NodeListOf<HTMLImageElement>} */ (
-    document.querySelectorAll('img[src$=".webp"]')
-  );
-  images.forEach(img => {
-    img.src = img.src.replace(/\.webp$/i, ".png");
-  });
-  console.log("POLYFILL: Using PNG instead of WEBP");
-}
-
 window.addEventListener("DOMContentLoaded", () => {
-  // Feature detection for CSS nesting
+  //POLYFILL for CSS nesting
   if (!CSS.supports("selector(&)")) {
     // CSS Nesting not supported.  Load alternative CSS file
     const link = /** @type {HTMLLinkElement} */ (
@@ -30,18 +11,20 @@ window.addEventListener("DOMContentLoaded", () => {
     console.log("POLYFILL: Using FLAT CSS instead of Nested");
   }
 
-  supportsWebP(function (supported) {
-    if (!supported) {
-      replaceWebPWithPNG();
-    }
-  });
+  //POLYFILL for WEBP to PNG
+  const webP = new Image();
+  webP.onload = webP.onerror = function () {
+    if (webP.height !== 1) {
+      // Replace WEBP with PNG
 
-  // Polyfill for WEBP
-  if (!CSS.supports("selector(&)")) {
-    // CSS Nesting not supported.  Load alternative CSS file
-    const link = /** @type {HTMLLinkElement} */ (
-      document.getElementById("main-stylesheet")
-    );
-    link.href = link.href.replace("min", "flat");
-  }
+      /** @type {NodeListOf<HTMLImageElement>} */ (
+        document.querySelectorAll('img[src$=".webp"]')
+      ).forEach(img => {
+        img.src = img.src.replace(/\.webp$/i, ".png");
+      });
+      console.log("POLYFILL: Using PNG instead of WEBP");
+    }
+  };
+  webP.src =
+    "data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=";
 });
