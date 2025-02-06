bfe09bcec09464b53ae2bbb73aba927add142027
Carter Medlin <carterm@users.noreply.github.com>
2025-02-06 10:43:04 -0800

    data sample

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -230,7 +230,7 @@ module.exports = function (
 
   eleventyConfig.addDataExtension("csv", (/** @type {string} */ contents) =>
     parse(contents, {
-      columns: true,
+      columns: header => header.map(col => col.replace(/\s+/g, "_")),
       skip_empty_lines: true,
       cast: value =>
         ["true", "false"].includes(value) ? value === "true" : value //Boolean value parsing
