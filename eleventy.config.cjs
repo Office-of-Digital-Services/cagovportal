1ff53218db71ed6ae269526a9a83bc560b11e521
Carter Medlin <carterm@users.noreply.github.com>
2025-10-09 10:42:39 -0700

    redirect for gov letter

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -211,7 +211,7 @@ module.exports = function (
   // After build hook to create an XML sitemap for PDF files
   eleventyConfig.on("afterBuild", () => {
     const pdfDirs = [
-      path.join(__dirname, "_site", "images")
+      path.join(__dirname, "_site", "immigration/documents")
       //path.join(__dirname, "_site", "docs")
     ];
     const outputFilePath = path.join(__dirname, "_site", "sitemaps/pdf.xml");
