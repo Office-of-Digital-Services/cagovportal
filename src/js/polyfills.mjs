b729260eedf679e3c186de404efc111ea38a9452
Carter Medlin <carterm@users.noreply.github.com>
2025-12-02 10:29:21 -0800

    using correct filename for flat support

--- src/js/polyfills.mjs
+++ src/js/polyfills.mjs
@@ -1,21 +1,19 @@
 //@ts-check
 
 window.addEventListener("DOMContentLoaded", () => {
-  // POLYFILL for CSS nesting
-  document.addEventListener("DOMContentLoaded", () => {
-    if (!CSS.supports("selector(&)")) {
-      // CSS Nesting not supported. Load alternative CSS file
-      const links = document.querySelectorAll("link[rel='stylesheet']");
-      const link = Array.prototype.find.call(links, l =>
-        l.href.includes("/css/custom.")
-      );
+  //POLYFILL for CSS nesting
+  if (!CSS.supports("selector(&)")) {
+    // CSS Nesting not supported. Load alternative CSS file
+    const links = document.querySelectorAll("link[rel='stylesheet']");
+    const link = Array.prototype.find.call(links, l =>
+      l.href.includes("/css/cagov-custom.")
+    );
 
-      if (link) {
-        link.href = link.href.replace("min", "flat");
-        console.log("POLYFILL: Using FLAT CSS instead of Nested");
-      }
+    if (link) {
+      link.href = link.href.replace("min", "flat");
+      console.log("POLYFILL: Using FLAT CSS instead of Nested");
     }
-  });
+  }
 
   //POLYFILL for WEBP to PNG
   function supportsWebP() {
