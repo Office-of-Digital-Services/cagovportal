7dcdf1318ad0789e1750d3af17c99fee6b1b4e58
Carter Medlin <carterm@users.noreply.github.com>
2025-12-02 10:15:22 -0800

    proposed change

--- src/js/polyfills.mjs
+++ src/js/polyfills.mjs
@@ -12,21 +12,22 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   //POLYFILL for WEBP to PNG
-  const webP = new Image();
-  webP.onload = webP.onerror = function () {
-    if (webP.height !== 1) {
-      // Replace WEBP with PNG
-
-      /** @type {NodeListOf<HTMLImageElement>} */ (
-        document.querySelectorAll('img[src*=".webp" i]')
-      ).forEach(img => {
-        img.src = img.src
-          .replace(/\.jpg\.webp/i, ".jpg")
-          .replace(/\.webp/i, ".png");
-      });
-      console.log("POLYFILL: Using PNG instead of WEBP");
+  function supportsWebP() {
+    const canvas = document.createElement("canvas");
+    if (canvas.getContext && canvas.getContext("2d")) {
+      return canvas.toDataURL("image/webp").indexOf("data:image/webp") === 0;
     }
-  };
-  webP.src =
-    "data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=";
+    return false;
+  }
+
+  if (!supportsWebP()) {
+    // Replace WEBP with PNG
+    /** @type {NodeListOf<HTMLImageElement>} */
+    (document.querySelectorAll('img[src*=".webp" i]')).forEach(img => {
+      img.src = img.src
+        .replace(/\.jpg\.webp/i, ".jpg")
+        .replace(/\.webp/i, ".png");
+    });
+    console.log("POLYFILL: Using PNG instead of WEBP");
+  }
 });
