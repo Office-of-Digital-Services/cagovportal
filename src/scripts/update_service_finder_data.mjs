b29928afa786d8cf0889477fa1e870daa9792ed8
Carter Medlin <carterm@users.noreply.github.com>
2026-07-23 10:47:42 -0700

    service name sorg

--- src/scripts/update_service_finder_data.mjs
+++ src/scripts/update_service_finder_data.mjs
@@ -64,7 +64,7 @@ const TABLES = [
     tableId: "tblS8RYo4FSqmONyu",
     name: "services",
     fields: ["serviceId"],
-    sort: ["fldTefBeQ2PJgm4N4"], //ServiceId
+    sort: ["fldUE83DR6inhTAbn"], //Service Name
     fieldMapping: {
       serviceId: "fldTefBeQ2PJgm4N4"
     }
